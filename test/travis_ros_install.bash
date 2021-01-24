d6f5380feca146ba445963193225db790f513b91
T O <t_o@gmail.com>
2021-01-24 06:06:59 +0000

    add sudo

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,12 +1,10 @@
 #!/bin/bash -xve
 
-pip install --upgrade pip
-
 #required packages
-pip install catkin_pkg
-pip install empy
-pip install pyyaml
-pip install rospkg
+sudo pip install catkin_pkg
+sudo pip install empy
+sudo pip install pyyaml
+sudo pip install rospkg
 
 #ros install
 cd ..
